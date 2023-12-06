2b1a9fa46313aeb5f466050fb4cbc97708743f42
Youcefng <118141354+YOUCEFngg@users.noreply.github.com>
2023-12-06 17:48:16 +0100

    Add files via upload

---
+++ IHM/src/ihm/IHM.java
@@ -0,0 +1,16 @@
+
+package ihm;
+
+
+public class IHM {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Login  LoginFrame = new Login();
+         LoginFrame.setVisible(true);
+         LoginFrame.pack();
+         LoginFrame.setLocationRelativeTo(null);
+    }
+    
+}
